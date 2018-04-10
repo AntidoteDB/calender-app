2d5363dabf80dcc671ca4d70f37ec6423f734d6f
dilan400388 <dlnshaminda@gmail.com>
2018-04-10 23:17:43 +0200

    Working pop up for calendar 1 and 2

--- Application/public/js/appoinmentPopup.js
+++ Application/public/js/appoinmentPopup.js
@@ -1,15 +1,51 @@
-$(document).ready(function () {
-    initialize();  //initializes GUI
-	 
-	$( "#appInputForm-1" ).click(function() {
-		alert("clcik");
-        modal.style.display = "block";
-    });
-	
-	let span = document.getElementsByClassName("close")[0];
-	
-	span.onclick = function() {
-      modal.style.display = "none";
-    }
+
+$(document).ready(function(){
+var model = document.getElementById('modelPop-1');
+ 
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("close-1");
+ 
+$( "#addAppointment-1" ).click(function() {
+		model.style.display = "block";
 });
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    model.style.display = "none";
+}
+
+ 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == model) {
+        model.style.display = "none";
+    }
+}
+});
+
+
+$(document).ready(function(){
+var model2 = document.getElementById('modelPop-2');
+ 
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementById("close-2");
+ 
+$( "#addAppointment-2" ).click(function() {
+		model2.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+    model2.style.display = "none";
+}
+
+ 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == model) {
+        model2.style.display = "none";
+    }
+}
+});
