b202e046216d2d65c7d738cc178ee0ebc7421982
dilan400388 <dlnshaminda@gmail.com>
2018-04-12 18:36:39 +0200

    Changed the view to appear two calendars in a row
    Signed-off-by: dilan400388 <dlnshaminda@gmail.com>
    
    # Conflicts:
    #	Application/public/css/appoinment.css
    #	Application/public/js/appoinmentPopup.js
    #	Application/views/index.html

--- Application/public/js/appoinmentPopup.js
+++ Application/public/js/appoinmentPopup.js
@@ -1,51 +1,37 @@
 
+var model = "";
 $(document).ready(function(){
-var model = document.getElementById('modelPop-1');
+model = document.getElementById('modelPop-1');
  
-
-// Get the <span> element that closes the modal
 var span = document.getElementById("close-1");
- 
-$( "#addAppointment-1" ).click(function() {
-		model.style.display = "block";
-});
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     model.style.display = "none";
 }
 
- 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == model) {
-        model.style.display = "none";
-    }
+});
+
+
+function openModel_1()
+{
+	model.style.display = "block";
 }
-});
 
-
+var model2 = "";
 $(document).ready(function(){
-var model2 = document.getElementById('modelPop-2');
+model2 = document.getElementById('modelPop-2');
  
 
-// Get the <span> element that closes the modal
 var span2 = document.getElementById("close-2");
- 
-$( "#addAppointment-2" ).click(function() {
-		model2.style.display = "block";
-});
 
-// When the user clicks on <span> (x), close the modal
 span2.onclick = function() {
     model2.style.display = "none";
 }
 
- 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == model) {
-        model2.style.display = "none";
-    }
-}
 });
+
+ 
+ function openModel_2()
+{
+	model2.style.display = "block";
+}
